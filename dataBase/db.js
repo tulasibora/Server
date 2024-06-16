7d64ae4d2d63718815202d46894a16b86ba01c17
tulasibora <113518104+tulasibora@users.noreply.github.com>
2024-06-16 14:57:36 +0530

    hloo

--- dataBase/db.js
+++ dataBase/db.js
@@ -1,19 +1,16 @@
 import mysql from "mysql";
 import "dotenv/config";
 
-const con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-console.log({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+const mySqlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}:${process.env.MYSQLHOST}
+:${process.env.MYSQLPORT}:${process.env.MYSQLDATABASE}`;
+
+const con = mysql.createConnection(mySqlDB);
+// {
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// }
 
 con.connect(function (err) {
   if (err) {
