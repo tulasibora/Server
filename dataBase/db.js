947a732bd5d080729a5d9225d57e456742b9c69f
tulasibora <113518104+tulasibora@users.noreply.github.com>
2024-06-16 16:17:53 +0530

    add db configuration

--- dataBase/db.js
+++ dataBase/db.js
@@ -1,16 +1,12 @@
 import mysql from "mysql";
 import "dotenv/config";
 
-const mySqlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}:${process.env.MYSQLHOST}
-:${process.env.MYSQLPORT}:${process.env.MYSQLDATABASE}`;
-
-const con = mysql.createConnection(mySqlDB);
-// {
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// }
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 con.connect(function (err) {
   if (err) {
